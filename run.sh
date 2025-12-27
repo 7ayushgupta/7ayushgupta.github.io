1ce0e3d513ce9c2acab3d9237d3d8196f095e0e5
Ayush Gupta <7ayushgupta@gmail.com>
2025-12-27 21:02:40 +0530

    Change website formatting

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 
 # Simple script to run your minimalist portfolio website
 echo "🚀 Starting your minimalist portfolio website..."
-echo "📍 Website will be available at: http://localhost:8000"
+echo "📍 Website will be available at: http://localhost:8089"
 echo "🛑 Press Ctrl+C to stop the server"
 echo ""
 
 # Start the local server
-python3 -m http.server 8000
+python3 -m http.server 8089
